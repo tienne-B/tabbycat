cd483c609585f58b5ddec63b825ebdb85b5d34f9
Étienne Beaulé <beauleetienne0@gmail.com>
2020-12-17 01:32:18 -0500

    Add cache invalidation on setting current round

--- tabbycat/tournaments/forms.py
+++ tabbycat/tournaments/forms.py
@@ -1,3 +1,5 @@
+from django.core.cache import cache
+from django.db import connection
 from django.forms import CharField, ChoiceField, Form, ModelChoiceField, ModelForm
 from django.forms.fields import IntegerField
 from django.forms.models import ModelChoiceIterator
@@ -13,6 +15,7 @@ from options.preferences import TournamentStaff
 from options.presets import all_presets, get_preferences_data, presets_for_form, public_presets_for_form
 
 from .models import Round, Tournament
+from .signals import update_tournament_cache
 from .utils import auto_make_rounds
 
 
@@ -162,6 +165,12 @@ class RoundWithCompleteOptionField(RoundField):
         return super().to_python(value)
 
 
+def clear_cache_all_rounds(tournament):
+    cache.delete_many(
+        ["%s_%s_%s_%s" % (connection.schema_name, tournament.slug, r.seq, 'object') for r in tournament.round_set.all()])
+    update_tournament_cache(Tournament, tournament)
+
+
 class SetCurrentRoundSingleBreakCategoryForm(Form):
     """Form to set completed rounds in a tournament with a single break category."""
 
@@ -177,6 +186,7 @@ class SetCurrentRoundSingleBreakCategoryForm(Form):
         seq = self.cleaned_data['current_round'].seq
         self.tournament.round_set.filter(seq__lt=seq).update(completed=True)
         self.tournament.round_set.filter(seq__gte=seq).update(completed=False)
+        clear_cache_all_rounds(self.tournament)
 
 
 class SetCurrentRoundMultipleBreakCategoriesForm(Form):
@@ -229,7 +239,6 @@ class SetCurrentRoundMultipleBreakCategoriesForm(Form):
             self.tournament.prelim_rounds().filter(seq__lt=seq).update(completed=True)
             self.tournament.prelim_rounds().filter(seq__gte=seq).update(completed=False)
             self.tournament.break_rounds().update(completed=False)
-
         else:
             self.tournament.prelim_rounds().update(completed=True)
             for category in self.tournament.breakcategory_set.all():
@@ -240,3 +249,4 @@ class SetCurrentRoundMultipleBreakCategoriesForm(Form):
                     seq = self.cleaned_data['elim_' + category.slug].seq
                     category.round_set.filter(seq__lt=seq).update(completed=True)
                     category.round_set.filter(seq__gte=seq).update(completed=False)
+        clear_cache_all_rounds(self.tournament)
