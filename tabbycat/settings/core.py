1b8dfab251517dbf54d2581e69eb54bfd23d69fb
Étienne Beaulé <beauleetienne0@gmail.com>
2021-11-02 23:40:51 -0300

    Disable global preferences in templates

--- tabbycat/settings/core.py
+++ tabbycat/settings/core.py
@@ -181,7 +181,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # for Jet
                 'utils.context_processors.debate_context',  # for tournament config vars
                 'django.template.context_processors.i18n',  # for serving static language translations
-                'dynamic_preferences.processors.global_preferences',
+                #'dynamic_preferences.processors.global_preferences',
             ],
             'loaders': [
                 ('django.template.loaders.cached.Loader', [
