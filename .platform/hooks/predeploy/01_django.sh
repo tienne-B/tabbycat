66e94805cbf519db25addde204b12793816d22fe
Étienne Beaulé <beauleetienne0@gmail.com>
2022-03-06 12:57:52 -0400

    Don't migrate on all deploys

--- .platform/hooks/predeploy/01_django.sh
+++ .platform/hooks/predeploy/01_django.sh
@@ -4,11 +4,12 @@ cd /var/app/staging/tabbycat
 export PYTHONPATH=/var/app/venv/staging-LQM1lest/bin:/var/app/staging/tabbycat
 export DJANGO_SETTINGS_MODULE=settings
 
-echo "-----> Running database migration"
-python manage.py migrate_schemas --noinput
+echo "-----> Running database migration - Skipped"
+#python manage.py migrate_schemas --noinput
 
 echo "-----> Running dynamic preferences checks"
 python manage.py checkpreferences_schemas --tenant
 
 echo "-----> Running static files compilation"
+npm run build
 python manage.py collectstatic --noinput
