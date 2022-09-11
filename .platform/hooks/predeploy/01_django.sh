1b68551626551963fefa383d67b0468f759ef742
Étienne Beaulé <beauleetienne0@gmail.com>
2022-09-11 12:36:37 -0300

    Install node/npm

--- .platform/hooks/predeploy/01_django.sh
+++ .platform/hooks/predeploy/01_django.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /var/app/staging/tabbycat
-pipenv shell
+# pipenv shell
 # export PYTHONPATH=/var/app/venv/staging-LQM1lest/bin:/var/app/staging/tabbycat
 export DJANGO_SETTINGS_MODULE=settings
 
@@ -12,6 +12,6 @@ echo "-----> Running database migration - Skipped"
 echo "-----> Running dynamic preferences checks"
 python manage.py checkpreferences_schemas --tenant
 
-echo "-----> Running static files compilation"
-npm run build
-python manage.py collectstatic --noinput
+echo "-----> Running static files compilation - Skipped"
+#npm run build
+#python manage.py collectstatic --noinput
