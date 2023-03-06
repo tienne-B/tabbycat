cf695f8d544d5c488ae000e2bb8466db47728cce
Étienne Beaulé <beauleetienne0@gmail.com>
2023-03-06 16:34:45 -0400

    Add logging for portal consumer

--- tabbycat/portal/consumers.py
+++ tabbycat/portal/consumers.py
@@ -2,8 +2,6 @@ import logging
 from subprocess import PIPE, Popen
 
 from channels.consumer import SyncConsumer
-from django.db import connection
-from django_tenants.utils import schema_context
 
 from .models import Client
 from .utils import get_postgres_url
@@ -14,22 +12,9 @@ logger = logging.getLogger(__name__)
 class PortalQueueConsumer(SyncConsumer):
 
     def create_schema(self, event):
+        logger.info("Creating new schema: %s" % (event['client'],))
         client = Client.objects.get(id=event.get('client'))
-
         client.create_schema()
-
-        # Copy user to new site
-        if client.user is not None:
-            user_model = client.user.__class__
-            with connection.cursor() as cursor:
-                cursor.execute(
-                    "INSERT INTO " + client.schema_name + ".auth_user SELECT * FROM public.auth_user WHERE id=%d" %
-                    client.user.pk)
-                cursor.execute(
-                    "INSERT INTO " + client.schema_name + ".authtoken_token SELECT * FROM public.authtoken_token WHERE user_id=%d" %
-                    client.user.pk)
-            with schema_context(client.schema_name):
-                user_model.objects.filter(pk=client.user.pk).update(is_staff=True, is_superuser=True)
 
 
 class DatabaseBackupConsumer(SyncConsumer):
