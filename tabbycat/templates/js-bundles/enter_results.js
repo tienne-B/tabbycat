9fd08c6a9d8dbad939ddc6f4e91cf93c89bd5f60
Étienne Beaulé <beauleetienne0@gmail.com>
2024-06-28 09:10:50 -0300

    Non-strict equality

--- tabbycat/templates/js-bundles/enter_results.js
+++ tabbycat/templates/js-bundles/enter_results.js
@@ -19,7 +19,7 @@ function refresh_totals(scoresheet) {
   // Fix the branching logic here into something cleaner
   var allClasses = 'btn-dark btn-secondary btn-success btn-primary btn-warning btn-danger btn-info';
 
-  if ("{{ pref.teams_in_debate }}" === 2) {
+  if ("{{ pref.teams_in_debate }}" == 2) {
     // 2-team
     $aff_total = $('.0_total', $scoresheet);
     $neg_total = $('.1_total', $scoresheet);
